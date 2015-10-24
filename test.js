7a2513054455ad9d0573df1c0bdbee902a4ce8af
Andres Suarez <zertosh@gmail.com>
2015-10-24 14:55:31 -0700

    use builtin stream instead of through2

--- test.js
+++ test.js
@@ -1,28 +1,25 @@
 var File = require('vinyl');
 var fs = require('fs');
+var stream = require('stream');
 var test = require('tape');
-var through = require('through2');
+
+var source = require('./');
 
 test('vinyl-source-buffer', function(t) {
-
   t.plan(3);
 
-  var source = require('./');
+  var filename = __filename;
+  var src = fs.readFileSync(filename, 'utf8');
 
-  fs.createReadStream(__filename)
-    .pipe(source(__filename))
-    .pipe(through.obj(function(file, enc, next) {
+  var testStream = new stream.Transform({objectMode: true});
+  testStream._transform = function transform(file, enc, next) {
+    t.ok(file instanceof File, 'file is a vinyl instance');
+    t.equal(file.path, filename, 'filename matches');
+    t.equal(file.contents.toString(), src, 'file contents matches');
+    t.end();
+  };
 
-      t.ok(file instanceof File, 'file is a vinyl instance');
-
-      t.equal(file.path, __filename, 'filename matches');
-
-      t.equal(
-        file.contents.toString(),
-        fs.readFileSync(file.path, 'utf8'),
-        'file contents matches');
-
-      t.end();
-    }));
-
+  fs.createReadStream(filename)
+    .pipe(source(filename))
+    .pipe(testStream);
 });
